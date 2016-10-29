ed1f0e46931370a1bd24f35a479c806c65c856d4
InstantCrowd <instantcrowd@gmail.com>
2016-10-29 23:38:21 +0200

    created quote

---
+++ src/main/java/hello/Application.java
@@ -0,0 +1,22 @@
+package hello;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.support.SpringBootServletInitializer;
+
+/**
+ * Created by dimi on 29.10.2016.
+ */
+
+@SpringBootApplication
+public class Application extends SpringBootServletInitializer{
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application){
+        return application.sources(Application.class);
+    }
+
+    public static void main(String[] args){
+        SpringApplication.run(Application.class,args);
+    }
+}
--- src/main/java/hello/Quote.java
+++
@@ -1,7 +0,0 @@
-package hello;
-
-/**
- * Created by dimi on 29.10.2016.
- */
-public class Quote {
-}
